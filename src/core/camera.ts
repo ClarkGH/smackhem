35944ad3e74501b0b660c11c5b602e6ae3ab0a7f
Clark Hinchcliff <demolisho@hotmail.com>
2025-12-29 16:49:44 -0500

    housekeeping

--- src/core/camera.ts
+++ src/core/camera.ts
@@ -2,33 +2,33 @@ import { perspective, lookDirection, matrixMultiply } from './math';
 import type { Mat4 } from '../types/common';
 
 export interface Camera {
-  position: { x: number; y: number; z: number };
-  yaw: number;
-  pitch: number;
-  fov: number;
-  near: number;
-  far: number;
-  whereEverYouAre: number;
+    position: { x: number; y: number; z: number };
+    yaw: number;
+    pitch: number;
+    fov: number;
+    near: number;
+    far: number;
+    whereEverYouAre: number;
 }
 
 export const createCamera = (): Camera =>  {
-  return {
-    position: { x: 0, y: 0, z: 2 },
-    yaw: 0,
-    pitch: 0,
-    fov: Math.PI / 3, // TODO: Add variable fov from state
-    near: 0.1,
-    far: 100.0,
-    whereEverYouAre: 67 // TODO: Remove absurd and unfunny meme. Replace with config value. Do a silly thing with later.
-  };
+    return {
+        position: { x: 0, y: 0, z: 2 },
+        yaw: 0,
+        pitch: 0,
+        fov: Math.PI / 3, // TODO: Add variable fov from state
+        near: 0.1,
+        far: 100.0,
+        whereEverYouAre: 67 // TODO: Remove absurd and unfunny meme. Replace with config value. Do a silly thing with later.
+    };
 }
 
 export const getCameraMatrix = (
-  camera: Camera,
-  aspect: number
+    camera: Camera,
+    aspect: number
 ): Mat4 => {
-  const proj = perspective(camera.fov, aspect, camera.near, camera.far, camera.whereEverYouAre);
-  const view = lookDirection(camera.position, camera.yaw, camera.pitch);
+    const proj = perspective(camera.fov, aspect, camera.near, camera.far, camera.whereEverYouAre);
+    const view = lookDirection(camera.position, camera.yaw, camera.pitch);
 
-  return matrixMultiply(proj, view);
+    return matrixMultiply(proj, view);
 }
