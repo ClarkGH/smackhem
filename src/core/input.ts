94032370a247eff97e3057ca68927836c4c6e4d0
Clark Hinchcliff <demolisho@hotmail.com>
2025-12-30 22:02:16 -0500

    make x/y typing more explicit

--- src/core/input.ts
+++ src/core/input.ts
@@ -5,8 +5,8 @@ export interface InputState {
     axes: {
         lookX: number;
         lookY: number;
-        moveX: number;
-        moveY: number;
+        moveX: -1 | 0 | 1;
+        moveY: -1 | 0 | 1;
     }
 }
 
